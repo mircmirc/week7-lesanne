38491288aa7c94571b01a288282ed55f6c727e77
mircmirc <tiit.tellis@torg.ee>
2025-02-14 01:32:17 +0200

    Add week 7 ülesanne files

---
+++ app.js
@@ -0,0 +1,20 @@
+const displayResult = document.querySelector('#result');
+const btnGetAPrediction = document.querySelector("button");
+
+const options = [
+    'You will fall in love this afternoon.', 
+    'You will win a lottery.',
+    'You will lose your phone. Check your left pocket.',
+    'You will fall asleep on a couch.',
+    'You will be taking a ring to Mordor.',
+    'You will get your letter from Hogwarts.',
+    'Elon Musk will buy TikTok.',
+    'Your crush will be mad at you, or give you a kiss. I am just a programm, I have no idea of what will happen in the future.'
+];
+
+btnGetAPrediction.addEventListener('click', () => {
+
+    const randomIndex = Math.floor(Math.random() * options.length);
+    const prediction = options[randomIndex];
+    displayResult.innerHTML = prediction;
+});
